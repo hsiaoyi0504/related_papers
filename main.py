fed3be231adb96ff93dcec683618f25a6a4d944e
Yi Hsiao <hsiaoyi0504@gmail.com>
2019-02-27 01:49:51 +0800

    batch efetch

--- main.py
+++ main.py
@@ -9,44 +9,55 @@ Entrez.email = 'hsiaoyi0504@gmail.com'
 
 
 def get_recent_articles():
+    # TODO: handle case with over 100000 result
     search_handler = Entrez.esearch(
-        db='pubmed', term='Circadian Rhythm[ALL] OR Circadian Clocks[ALL]', reldate=60, retmax=100000)
+        db='pubmed', term='2015:2018[CRDT]', retmax=100000, usehistory='y')
     record = Entrez.read(search_handler)
-    return record['IdList']
+    return (record['IdList'], record['WebEnv'], record['QueryKey'])
 
 
-def filter_articles(pubmed_ids, mesh_terms, threshold):
+def filter_articles(pubmed_ids, webenv, query_key, mesh_terms, threshold):
     filtered_ids = []
     clf, word_dict = pickle.load(open('model.pkl', 'rb'))
-    for pubmed_id in pubmed_ids:
+    count = int(len(pubmed_ids))
+    batch_size = 10000
+    # TODO: batch prediction
+    for start in range(0, count, batch_size):
+        # maximum retmax of efetch is 10000
         fetch_handler = Entrez.efetch(
-            db='pubmed', rettype='medline', retmode='text', id=pubmed_id)
-        record = Medline.read(fetch_handler)
-        record_mesh_terms = record.get('MH')
-        if record_mesh_terms is not None:
-            count = 0
-            for mesh in mesh_terms:
-                if mesh in record_mesh_terms:
-                    count += 1
-            if count >= threshold:
-                filtered_ids.append(pubmed_id)
-                continue
-        abstract = record.get('AB')
-        if abstract is not None:
-            words = abstract2words(abstract)
-            c = Counter(words)
-            values = []
-            for i in range(len(word_dict)):
-                values.append(c[word_dict[i]])
-            total_words = sum(values)
-            values = [v / total_words for v in values]
-            values = np.array(values).reshape(1, -1)
-            # TODO: set a cutoff to ensure the predicted value is useful
-            if clf.predict(values) == 1:
-                filtered_ids.append(pubmed_id)
-        else:
-            # TODO: handle articles without abstract
-            pass
+                db='pubmed', rettype='medline', retmode='text',
+                retstart=start, retmax=10000,
+                webenv=webenv, query_key=query_key)
+        records = Medline.parse(fetch_handler)
+        for record in records:
+            pubmed_id = record['PMID']
+            record_mesh_terms = record.get('MH')
+            if record_mesh_terms is not None:
+                count = 0
+                for mesh in mesh_terms:
+                    if mesh in record_mesh_terms:
+                        count += 1
+                if count >= threshold:
+                    filtered_ids.append(pubmed_id)
+                    continue
+            abstract = record.get('AB')
+            if abstract is not None:
+                words = abstract2words(abstract)
+                c = Counter(words)
+                values = []
+                for i in range(len(word_dict)):
+                    values.append(c[word_dict[i]])
+                total_words = sum(values)
+                if total_words == 0:
+                    continue
+                values = [v / total_words for v in values]
+                values = np.array(values).reshape(1, -1)
+                # TODO: set a cutoff to ensure the predicted value is useful
+                if clf.predict(values) == 1:
+                    filtered_ids.append(pubmed_id)
+            else:
+                # TODO: handle articles without abstract
+                pass
     return filtered_ids
 
 
@@ -60,6 +71,6 @@ if __name__ == '__main__':
         'Statistics as Topic/*methods', 'Transcription, Genetic/genetics'
     ]
     THRESHOLD = 3
-    pubmed_ids = get_recent_articles()
-    pubmed_ids = filter_articles(pubmed_ids, MeSH_TERMS, THRESHOLD)
+    pubmed_ids, webenv, query_key = get_recent_articles()
+    pubmed_ids = filter_articles(pubmed_ids, webenv, query_key, MeSH_TERMS, THRESHOLD)
     print(pubmed_ids)
